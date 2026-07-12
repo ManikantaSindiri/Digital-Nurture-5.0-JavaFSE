a38c4cde11c11563d57633d25b2daafc3cf5b8f0
manish <sindi@MSI>
2026-07-12 16:48:09 +0530

    Tdd3

--- week-1/TDD/Arrange_Act_Asert/src/main/java/Calculator.java
+++ week-1/TDD/Arrange_Act_Asert/src/main/java/Calculator.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
--- week-1/TDD/Arrange_Act_Asert/src/test/java/CalculatorTest.java
+++ week-1/TDD/Arrange_Act_Asert/src/test/java/CalculatorTest.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        System.out.println("Setup: Creating Calculator object");
+        calculator = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Teardown: Cleaning up");
+        calculator = null;
+    }
+
+    @Test
+    public void testAddition() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
 }
